a83e8e437886dce045a384dc3e96db945ef4cb8c
Lorenzo301203 <lorenzocoluccia99@gmail.com>
2024-10-24 19:39:55 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,17 +1,26 @@
-function preload() {
-  // put preload code here
-}
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
-  const message =
-    "This is a template repository\nfor the course Laboratorio di Computergrafica\nCommunication Design, Politecnico di Milano";
-  textAlign(CENTER, CENTER);
-  textSize(16);
-  text(message, width / 2, height / 2);
+  noLoop(); // Serve per disegnare una sola volta, rimuovi se vuoi disegnare continuamente
+  noStroke();
+  fill(150, 80, 60, 150); // Colore marrone semitrasparente
 }
 
 function draw() {
-  // put drawing code here
+  background(255);
+  let squareSize = 50; // Dimensione dei quadrati
+  let columns = Math.floor(width / squareSize);
+  let rows = Math.floor(height / squareSize);
+
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < rows; j++) {
+      let x = i * squareSize + random(-10, 10);
+      let y = j * squareSize + random(-10, 10);
+      rect(x, y, squareSize, squareSize);
+    }
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  redraw();
 }
